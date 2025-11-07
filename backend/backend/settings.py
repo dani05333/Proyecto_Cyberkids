9920fb7f034b8c774b6b6a127c48eeb0ff20eeea
th3IV <136734085+th3IV@users.noreply.github.com>
2025-11-06 23:31:03 -0300

    Change database engine to MySQL
    Updated database settings to use MySQL instead of SQLite.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CyberKidsDB',
+        'USER': 'root',
+        'PASSWORD': 'zwiKmNEXAlISHutzohJjzRuTiUsplJyE',
+        'HOST': 'turntable.proxy.rlwy.net',
+        'PORT': '56789',
     }
 }
 
